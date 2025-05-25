1a1274039ae3f615f9ac569b662998ad220e283f
Smarita <smaritad@gmail.com>
2025-05-25 22:08:22 +0530

    Some souts added

--- src/main/java/com/company/CompanyWebService/Controller/CompanyController.java
+++ src/main/java/com/company/CompanyWebService/Controller/CompanyController.java
@@ -40,7 +40,7 @@ public class CompanyController {
 
 	@PostMapping(value="/createCompany")
 	public GenericResponse createCompany( @RequestHeader("Authorization") String authorization,@RequestHeader("Role") String role,  @RequestBody Company company) {
-
+		System.out.println("inside createCompany()");
 		GenericResponse genericResponse;
 		System.out.println("Authorization: "+authorization);
 		System.out.println("Role: "+role);
@@ -64,7 +64,7 @@ public class CompanyController {
 	public GenericResponse viewCompany( @RequestHeader("Authorization") String authorization,@RequestHeader("Role") String role) {
 		System.out.println("Authorization: "+authorization);
 		System.out.println("Role: "+role);
-	
+		System.out.println("***");
 		if (role.equalsIgnoreCase("admin")) {
 			return companyservice.viewCompanyService();
 			//return new ResponseEntity<>(companyservice.viewCompanyService(), HttpStatus.OK);
